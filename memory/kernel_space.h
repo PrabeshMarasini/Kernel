a7567791f6a2892df20cc5f7811d06c152c1d5ee
Prabesh Marasini <95348153+PrabeshMarasini@users.noreply.github.com>
2024-06-18 16:47:32 +0545

    Delete memory directory

--- memory/kernel_space.h
+++
@@ -1,8 +0,0 @@
-#ifndef KERNEL_SPACE_H
-#define KERNEL_SPACE_H
-
-#include "common.h"
-
-VirtualMemory allocate_kernel_space();
-
-#endif // KERNEL_SPACE_H
--- memory/user_space.h
+++
@@ -1,9 +0,0 @@
-#ifndef USER_SPACE_H
-#define USER_SPACE_H
-
-#include "common.h"
-
-VirtualMemory allocate_user_space(size_t space_size);
-VirtualMemory* allocate_multiple_user_spaces(size_t count);
-
-#endif // USER_SPACE_H
